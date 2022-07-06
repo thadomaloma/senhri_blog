9b04927184a72f4a795429159d2912ee6e98b19b
Thado Zalawmpuia <zlwmpthado@gmail.com>
2022-07-06 17:43:45 +0900

    sign up function bug fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users, controllers: { registrations: 'users/registrations', sessions: 'users/sessions' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   get '/users', to: 'users#index'
   get '/users/:id', to: 'users#show'
   get '/friends', to: 'friendships#index'
