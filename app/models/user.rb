50c270951630269ced226be39aaf962b0f5132d2
Thado Zalawmpuia <zlwmpthado@gmail.com>
2022-07-06 17:51:37 +0900

    Deleted facebook login

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,14 +15,6 @@ class User < ApplicationRecord
   validates :gender, presence: true, unless: -> { from_facebook? }
   validates :birthdate, presence: true, unless: -> { from_facebook? }
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
-      user.profile_pic = auth.info.image
-    end
-  end
-
   def self.new_with_session(params, session)
     super.tap do |user|
       if (data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info'])
